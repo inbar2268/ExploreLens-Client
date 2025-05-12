c09b966288a0f91bc2576c27aee9df474a2e95c2
Hadar <hadar5445@gmail.com>
2025-05-12 21:33:43 +0300

    fetch nearby places

--- app/src/main/java/com/example/explorelens/data/network/NearbyPlacesApi.kt
+++ app/src/main/java/com/example/explorelens/data/network/NearbyPlacesApi.kt
@@ -1,13 +1,12 @@
 package com.example.explorelens.data.network
 
 import com.example.explorelens.data.model.PointOfIntrests.PointOfInterest
-import com.google.type.LatLng
 import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface NearbyPlacesApi {
-    @GET("/places/nearby/mock")
+    @GET("/places/nearby/mock/")
     suspend fun getNearbyPlaces(
         @Query("lat") latitude: Double,
         @Query("lng") longitude: Double,
