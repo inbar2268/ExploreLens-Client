0e815ba5d6eb8ab484ff71b154d95d37de90330f
noa1608 <noa160802@gmail.com>
2025-06-04 17:09:03 +0300

    fix margin

--- app/src/main/java/com/example/explorelens/ar/AppRenderer.kt
+++ app/src/main/java/com/example/explorelens/ar/AppRenderer.kt
@@ -638,7 +638,7 @@ class AppRenderer(
                     y = 0.5f,
                     siteName = "Colosseum"
                 ),
-                siteInfoId = "6818fd47b249f52360e546ec",
+                siteInfoId = "6817c17ec96f0e5280196da6",
             )
 
             launch(Dispatchers.Main) {
@@ -793,7 +793,7 @@ class AppRenderer(
                 val dZ = closestPointZ - anchorPos[2]
                 val perpendicularDistance = sqrt(dX * dX + dY * dY + dZ * dZ)
 
-                val touchThreshold = 0.2f
+                val touchThreshold = 0.7f
 
                 if (dotProduct > 0 && perpendicularDistance < touchThreshold) {
                     if (distanceToAnchor < closestDistance) {
