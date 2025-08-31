ad3df3aa100d774a0d23ee80202898b75d3ad9e9
inbar2268 <inbar2268@gmail.com>
2025-08-31 15:58:54 +0300

    change files names and  location for better structure

--- app/src/main/java/com/example/explorelens/ar/components/ARAnchorsLiveData.kt
+++ app/src/main/java/com/example/explorelens/ar/components/ARAnchorsLiveData.kt
@@ -2,6 +2,7 @@ package com.example.explorelens.model
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import com.example.explorelens.data.model.siteDetectionData.ARLabeledAnchor
 import java.util.concurrent.CopyOnWriteArrayList
 
 object ARAnchorsManager {
